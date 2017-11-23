31d58c3151f8421c45dc58aae2e48eb14b252845
Brian Peterson <brianpeterson28@gmail.com>
2017-11-22 19:57:15 -0600

    Added Python File
    Created python script file. Wrote DocString.

---
+++ league_builder.py
@@ -0,0 +1,19 @@
+'''
+Brian Peterson | bpeterson28 |  
+
+Treehouse Tech Degree - Project 1 - Build A Soccer League
+
+This script opens a csv file containing soccer player information and uses
+that information to automatically sort the players into three teams. It also 
+automatically generates welcome letters for each player's parent/legal 
+gaurdian.  
+
+Teams = (1) Sharks, (2) Dragons, and (3) Raptors. 
+'''
+
+import csv
+
+
+
+
+if __name__ == "__main__":
